90af7fc7ea435e557aaac642561028cef4a8d2ad
hugovk <hugovk@users.noreply.github.com>
2016-05-13 14:45:59 +0300

    #NoAtSignIn6Words

--- inxwordsloop.sh
+++ inxwordsloop.sh
@@ -2,7 +2,7 @@
 
 while true; do
     echo "$(date)"
-    python @inxwords.py -nw --loop -d 1 -y /home/botuser/bin/data/inxwords.yaml
+    python inxwords.py -nw --loop -d 1 -y /home/botuser/bin/data/inxwords.yaml
     echo "$(date)"
     sleep 60
 done
